44f1227f39dbeac595a64562dd59063421cbe470
HudsonWayne <waynewaynebenhura@gmail.com>
2024-08-02 10:25:02 +0200

    using the .find to find the index of every latter in the variable

--- FirstFile.py
+++ FirstFile.py
@@ -1,9 +1,5 @@
 #Multiple Assignment = Allows you to assign multiple variable at the same line of code
 
 name = "Wayne"
-age = 30;
-attractive = True
-
-print (age)
-print (name)
-print (attractive)
+#print(len(name))
+print(name.find("e"))
